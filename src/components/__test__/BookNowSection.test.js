33b0a0bd5fdba94f476fbdfe5518d1a7a6af053a
Madhushri S. Kulkarni <88674169+madhushreesk@users.noreply.github.com>
2023-05-29 15:56:10 +0530

    added more test cases

--- src/components/__test__/BookNowSection.test.js
+++ src/components/__test__/BookNowSection.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import BookNowSection from '../BookNowSection';
 
 
@@ -8,4 +9,19 @@ describe("Test the BookNowSection component", () =>{
         const buttonList = await screen.findAllByRole("button");
         expect(buttonList).toHaveLength(1);
     })
+
+    test("email input field should accept email",()=>{
+        render(<BookNowSection/>);
+        const email = screen.getByPlaceholderText("Enter your full name");
+        userEvent.type(email,"msk");
+        expect(email.value).not.toMatch("msk@gmail.com");
+    })
+
+    test("phone number field must contain numbers",()=>{
+        render(<BookNowSection/>);
+        const number = screen.getByPlaceholderText("Enter your phone number");
+        userEvent.type(number,"123");
+        expect(number.value).not.toMatch("1234");
+    })
 });
+
